50e0e603a0c45ff1ad35d3e18e4618542920f7f0
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-03-25 17:54:55 +0530

    feat: setted up authjs for authenticaition

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import Credentials from "next-auth/providers/credentials";
 import connectDb from "./lib/db";
 import User from "./models/user.model";
 import bcrypt from "bcryptjs";
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -42,8 +43,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         };
       },
     }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
   ],
   callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === "google") {
+        await connectDb();
+        const dbUser = await User.findOne({ email: user.email });
+        if (!dbUser) {
+          await User.create({
+            name: user.name,
+            email: user.email,
+          });
+        }
+        ((user.id = dbUser._id), (user.role = dbUser.AriaRole));
+      }
+      return true;
+    },
     async jwt({ token, user }) {
       ((token.name = user.name),
         (token.id = user.id),
@@ -51,16 +70,23 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         (token.role = user.role));
       return token;
     },
-    async session({token,session}){
-        if(session.user){
-            session.user.name = token.name,
-        session.user.id = token.id as string,
-        session.user.email = token.email as string,
-        session.user.role = token.role as string
-        }
-        return session
-
-
-    }
+    async session({ token, session }) {
+      if (session.user) {
+        ((session.user.name = token.name),
+          (session.user.id = token.id as string),
+          (session.user.email = token.email as string),
+          (session.user.role = token.role as string));
+      }
+      return session;
+    },
   },
+  pages: {
+    signIn: "/signin",
+    error: "/signin",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 10 * 24 * 60 * 60,
+  },
+  secret: process.env.AUTH_SCRET,
 });
