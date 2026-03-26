eda112fd5cb3b16bf18df2070fb9a0a15b029a94
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-03-26 17:39:03 +0530

    feat: Implement Redux for user state management, including an API endpoint to fetch user data and a hook to initialize it.

---
+++ src/app/api/user/me/route.ts
@@ -0,0 +1,23 @@
+import { auth } from "@/auth";
+import connectDb from "@/lib/db";
+import User from "@/models/user.model";
+
+export async function GET(req: Request) {
+  try {
+    await connectDb();
+    const session = await auth();
+    if (!session || !session.user) {
+      return Response.json(
+        { message: "User is not logged in" },
+        { status: 400 },
+      );
+    }
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return Response.json({ message: "User not found" }, { status: 404 });
+    }
+    return Response.json({ user }, { status: 200 });
+  } catch (error) {
+    return Response.json({ message: "Internal server error" }, { status: 500 });
+  }
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "@/lib/Provider";
+import ReduxProvider from "@/redux/ReduxProvider";
+import InitUser from "@/InitUser";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +32,12 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body>
-        <Provider>{children}</Provider>
+        <ReduxProvider>
+          <Provider>
+            <InitUser />
+          {children}
+          </Provider>
+        </ReduxProvider>
       </body>
     </html>
   );
