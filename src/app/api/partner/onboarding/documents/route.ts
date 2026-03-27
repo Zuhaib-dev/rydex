d356b4a3c8cfc5e29f439d2607e63f55ecbdd4f8
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-03-27 18:32:46 +0530

    feat: implement partner onboarding document upload endpoint, integrating Cloudinary for storage and updating user/vehicle records.

--- src/app/api/partner/onboarding/documents/route.ts
+++ src/app/api/partner/onboarding/documents/route.ts
@@ -1,11 +1,75 @@
+import { auth } from "@/auth";
+import uploadOnCloudnary from "@/lib/cloudinary";
 import connectDb from "@/lib/db";
+import User from "@/models/user.model";
+import Vehcile from "@/models/vehicle.model";
+import { NextRequest } from "next/server";
 
-export  async function POST(req:Request) {
-    try {
-        await connectDb()
-        
-    } catch (error) {
-        
+export async function POST(req: NextRequest) {
+  try {
+    await connectDb();
+    const session = await auth();
+    if (!session || !session.user?.email) {
+      return Response.json({ message: "User unauthorized " }, { status: 400 });
     }
-    
+    const user = await User.findOne({ email: session.user.email });
+    if (!user) {
+      return Response.json({ message: "User not found" });
+    }
+    const formdata = await req.formData();
+    const aadhar = (formdata.get("aadhar") as Blob) || null;
+    const drivingLicense = (formdata.get("drivingLicense") as Blob) || null;
+    const rc = (formdata.get("rc") as Blob) || null;
+    if (!aadhar || !drivingLicense || !rc) {
+      return Response.json(
+        { message: "All documents are required" },
+        { status: 400 },
+      );
+    }
+    const updatePayload: any = {
+      status: "pending",
+    };
+    if (aadhar) {
+      const url = await uploadOnCloudnary(aadhar);
+      if (!url) {
+        return Response.json(
+          { message: "aadhar Upload Failed" },
+          { status: 500 },
+        );
+      }
+      updatePayload.aadhar = url;
+    }
+    if (drivingLicense) {
+      const url = await uploadOnCloudnary(drivingLicense);
+      if (!url) {
+        return Response.json(
+          { message: "drivingLicense Upload Failed" },
+          { status: 500 },
+        );
+      }
+      updatePayload.drivingLicense = url;
+    }
+    if (rc) {
+      const url = await uploadOnCloudnary(rc);
+      if (!url) {
+        return Response.json({ message: "rc Upload Failed" }, { status: 500 });
+      }
+      updatePayload.rc = url;
+    }
+    const vehicle = await Vehcile.findOneAndUpdate(
+      { owner: session.user.id },
+      updatePayload,
+      { new: true },
+    );
+    if (!vehicle) {
+      return Response.json({ message: "Vehicle not found" }, { status: 400 });
+    }
+    if (user.partnerOnboardingSteps < 2) {
+      user.partnerOnboardingSteps = 2;
+    }
+    await user.save();
+    return Response.json({ message: "Documents uploaded successfully" });
+  } catch (error) {
+    return Response.json({ message: "Internal server error" }, { status: 500 });
+  }
 }
