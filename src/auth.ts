ca53484f73b20a7402d8f0e91ae408c43bf5d9c0
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-03-28 12:16:22 +0530

    feat: enable trustHost in NextAuth configuration

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,7 @@ import User from "./models/user.model";
 import bcrypt from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     Credentials({
       credentials: {
