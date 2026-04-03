a147fb5a3fe7abd41e1bd58faa4961c653f67c47
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-04-03 14:22:59 +0530

    feat: add admin route to approve partner onboarding and verify documents

---
+++ src/app/admin/reviews/partner/[id]/approve/route.ts
@@ -0,0 +1,59 @@
+import { auth } from "@/lib/auth";
+import connectDb from "@/lib/db";
+import PartnerBank from "@/models/partnerBank.model";
+import PartnerDocs from "@/models/partnerDocs.model";
+import User from "@/models/user.model";
+
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> },
+) {
+  try {
+    await connectDb();
+    const session = await auth();
+    if (!session || !session.user?.email || session.user?.role !== "admin") {
+      return Response.json({ message: "Unauthorized" }, { status: 400 });
+    }
+    await connectDb();
+    const partnerId = (await context.params).id;
+    const partner = await User.findById(partnerId);
+    if (!partner || partner.role !== "partner") {
+      return NextResponse.json(
+        { message: "Partner Not Found" },
+        { status: 400 },
+      );
+    }
+    if (partner.partnerStatus === "approved") {
+      return NextResponse.json(
+        { message: "Partner Already Approved" },
+        { status: 400 },
+      );
+    }
+    const partnerDocs = await PartnerDocs.findOne({ owner: partner._Id });
+    const partnerBank = await PartnerBank.findOne({ owner: partner._id });
+    if (!partnerDocs || !partnerBank) {
+      return NextResponse.json(
+        { message: "Partner Docs or Bank Not Found" },
+        { status: 400 },
+      );
+    }
+    partner.partnerStatus = "approved";
+    partner.partnerOnboardingSteps = 4;
+    await partner.save();
+    partnerDocs?.set({ status: "approved" });
+    partnerBank?.set({ status: "verified" });
+    await partnerDocs?.save();
+    await partnerBank?.save();
+    return NextResponse.json(
+      { message: "Partner Approved Successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: `Partner approval error: ${error}` },
+      { status: 500 },
+    );
+  }
+}
