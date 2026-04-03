2d9d72b0bd40de2e2b2a2f64d1e674adca1dc1aa
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-04-03 10:16:32 +0530

    feat: implement role-based dashboard routing for admin and partner users in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
-"use client"
+import AdminDashboard from "@/components/AdminDashboard";
 import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
+import PartnerDashboard from "@/components/PartnerDashboard";
 import PublicHome from "@/components/PublicHome";
-import { useSession } from "next-auth/react";
-export default function Home() {
-  const session = useSession()
+import { auth } from "@/lib/auth";
+
+export default async function Home() {
+  const session = await auth();
   return (
-    
-         <div className="w-full min-h-screen bg-white ">
-          <Nav />
-          <PublicHome />
-          <Footer />
-         </div>
+    <div className="w-full min-h-screen bg-white ">
+      <Nav />
+      {session?.user?.role == "partner" ? (
+        <PartnerDashboard />
+      ) : session?.user?.role == "admin" ? (
+        <AdminDashboard />
+      ) : (
+        <PublicHome />
+      )}
+      <Footer />
+    </div>
   );
 }
