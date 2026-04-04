a5dcd459bf0881bac05b8e475adc748f593c42c6
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-04-04 08:49:42 +0530

    fix: restrict admin middleware access to all routes instead of only /admin prefix

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,7 +24,7 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL("/", req.url));
   }
   const role = token.role;
-  if (pathname.startsWith("/admin")) {
+  if (pathname.startsWith("/")) {
     if (role != "admin") {
       return NextResponse.redirect(new URL("/", req.url));
     }
