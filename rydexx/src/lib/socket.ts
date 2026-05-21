fa64f588bf11f80ebe1e3ea6d6df7fd8fa4f4327
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-05-21 22:39:25 +0530

    refactor: standardize socket server initialization and remove direct environment variable usage

--- rydexx/src/lib/socket.ts
+++ rydexx/src/lib/socket.ts
@@ -1,10 +1,12 @@
 import { io, Socket } from "socket.io-client"
 
 let socket:Socket|null=null
+const SOCKET_SERVER =
+  process.env.NEXT_PUBLIC_SOCKET_SERVER || "https://rydex-nurn.onrender.com";
 
 export const getSocket=()=>{
 if(!socket){
-    socket=io(process.env.NEXT_PUBLIC_SOCKET_SERVER)
+    socket=io(SOCKET_SERVER.replace(/\/+$/, ""))
 }
 return socket
 }
