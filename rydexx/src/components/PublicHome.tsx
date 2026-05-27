5a5939ae363faa1d5b68c54f2eea8832b68adaa3
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-05-27 19:29:46 +0530

    feat: implement global styling improvements and expand PublicHome with new landing page sections

--- rydexx/src/components/PublicHome.tsx
+++ rydexx/src/components/PublicHome.tsx
@@ -3,13 +3,39 @@ import React, { useState } from "react";
 import HeroSection from "./HeroSection";
 import VehicleSlider from "./VehicleSlider";
 import AuthModel from "./AuthModel";
+import LandingFeatures from "./LandingFeatures";
+import LandingStats from "./LandingStats";
+import LandingHowItWorks from "./LandingHowItWorks";
+import LandingTestimonials from "./LandingTestimonials";
+import LandingCTA from "./LandingCTA";
 
 function PublicHome() {
   const [authOpen, setAuthOpen] = useState(false);
+  const openAuth = () => setAuthOpen(true);
+
   return (
     <>
-      <HeroSection  onAuthRequired={()=>setAuthOpen(true)} />
+      {/* 1. Cinematic hero with typewriter + parallax */}
+      <HeroSection onAuthRequired={openAuth} />
+
+      {/* 2. Dark bento features grid */}
+      <LandingFeatures />
+
+      {/* 3. Stats counter (light) */}
+      <LandingStats />
+
+      {/* 4. How it works (dark) */}
+      <LandingHowItWorks />
+
+      {/* 5. Vehicle fleet slider (light) */}
       <VehicleSlider />
+
+      {/* 6. Testimonials (light) */}
+      <LandingTestimonials />
+
+      {/* 7. Final CTA banner (dark) */}
+      <LandingCTA onAuthRequired={openAuth} />
+
       <AuthModel open={authOpen} onClose={() => setAuthOpen(false)} redirectTo="/user/book" />
     </>
   );
