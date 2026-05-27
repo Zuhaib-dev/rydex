564ffc201e629064bc0c1f054fa921c33d634b5d
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-05-27 17:53:07 +0530

    feat: add handleTimeout function to remove bookings from state upon expiration

--- rydexx/src/app/partner/pending-requests/page.tsx
+++ rydexx/src/app/partner/pending-requests/page.tsx
@@ -56,7 +56,10 @@ export default function VendorPendingPage() {
   const [bookings, setBookings] = useState<Booking[]>([]);
   const [loading, setLoading] = useState(true);
   const [processingId, setProcessingId] = useState<string | null>(null);
-   const router=useRouter()
+  const handleTimeout = (bookingId: string) => {
+    setBookings((prev) => prev.filter((b) => b._id !== bookingId));
+  };
+
   const fetchPendingBookings = async () => {
     if (!session?.user) {
       setLoading(false);
