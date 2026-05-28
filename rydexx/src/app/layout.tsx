3402c76dffecf52f1de13091f27f17f2a82ecd4b
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-05-28 23:33:05 +0530

    feat: add Microsoft Clarity analytics script to layout

--- rydexx/src/app/layout.tsx
+++ rydexx/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono, Syne } from "next/font/google";
 import "./globals.css";
 import Provider from "@/Provider";
@@ -186,6 +187,13 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wyaed16hp3");`}
+        </Script>
         <ReduxProvider>
           <Provider>
             <InitUser />
