0c47bf74c9a06e72eaa676ae2ad22971c9d561fa
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-05-30 22:05:25 +0530

    feat: add impersonation support to userSlice with admin state management

--- rydexx/src/redux/userSlice.ts
+++ rydexx/src/redux/userSlice.ts
@@ -2,9 +2,13 @@ import { IUser } from "@/models/user.model";
 import { createSlice } from "@reduxjs/toolkit";
 interface IUserState {
   userData: IUser | null;
+  adminUserData: IUser | null;
+  isImpersonating: boolean;
 }
 const initialState: IUserState = {
   userData: null,
+  adminUserData: null,
+  isImpersonating: false,
 };
 export const userSlice = createSlice({
   name: "user",
@@ -13,7 +17,19 @@ export const userSlice = createSlice({
     setUserData: (state, action) => {
       state.userData = action.payload;
     },
+    startImpersonation: (state, action) => {
+      state.adminUserData = state.userData;
+      state.userData = action.payload;
+      state.isImpersonating = true;
+    },
+    stopImpersonation: (state) => {
+      if (state.adminUserData) {
+        state.userData = state.adminUserData;
+      }
+      state.adminUserData = null;
+      state.isImpersonating = false;
+    },
   },
 });
-export const {setUserData} = userSlice.actions
+export const { setUserData, startImpersonation, stopImpersonation } = userSlice.actions;
 export default userSlice.reducer
