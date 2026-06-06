ef88b53ad1991e4f21913dd9b745654302040fb7
Zuhaib Rashid <xuvaxuva006@gmail.com>
2026-06-06 10:33:55 +0530

    fix: correct z-index value syntax in vehicle management modal

--- rydexx/src/components/admin/VehicleManagement.tsx
+++ rydexx/src/components/admin/VehicleManagement.tsx
@@ -835,7 +835,7 @@ export default function VehicleManagement() {
       {/* Reject / Suspend Reason Description Modal */}
       <AnimatePresence>
         {reasonModalOpen && (
-          <div className="fixed inset-0 z-[60] flex items-center justify-center p-4">
+          <div className="fixed inset-0 z-60 flex items-center justify-center p-4">
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
